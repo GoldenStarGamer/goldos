5c40353284570805c4a34a8e286637dd2fa60cf7
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-12-23 18:16:28 +0000

    added some stuff

--- main.c
+++ main.c
@@ -1,126 +1,63 @@
-/*
- * UEFI:SIMPLE - UEFI development made easy
- * Copyright ©️ 2014-2023 Pete Batard <pete@akeo.ie> - Public Domain
- * See COPYING for the full licensing terms.
- */
 #include <efi.h>
-#include <efilib.h>
-#include <libsmbios.h>
 
-#if defined(_M_X64) || defined(__x86_64__)
-static CHAR16* ArchName = L"x86 64-bit";
-#elif defined(_M_IX86) || defined(__i386__)
-static CHAR16* ArchName = L"x86 32-bit";
-#elif defined (_M_ARM64) || defined(__aarch64__)
-static CHAR16* ArchName = L"ARM 64-bit";
-#elif defined (_M_ARM) || defined(__arm__)
-static CHAR16* ArchName = L"ARM 32-bit";
-#elif defined (_M_RISCV64) || (defined(__riscv) && (__riscv_xlen == 64))
-static CHAR16* ArchName = L"RISC-V 64-bit";
-#else
-#  error Unsupported architecture
-#endif
-
-// Tri-state status for Secure Boot: -1 = Setup, 0 = Disabled, 1 = Enabled
-INTN SecureBootStatus = 0;
-
-/*
- * Query SMBIOS to display some info about the system hardware and UEFI firmware.
- * Also display the current Secure Boot status.
- */
-static EFI_STATUS PrintSystemInfo(VOID)
+// Entry point for the EFI application
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* st)
 {
-	EFI_STATUS Status;
-	SMBIOS_STRUCTURE_POINTER Smbios;
-	SMBIOS_STRUCTURE_TABLE* SmbiosTable;
-	SMBIOS3_STRUCTURE_TABLE* Smbios3Table;
-	UINT8 Found = 0, * Raw, * SecureBoot, * SetupMode;
-	UINTN MaximumSize, ProcessedSize = 0;
+    // Obtain a reference to the Console Output Interface
+    SIMPLE_TEXT_OUTPUT_INTERFACE* ConOut = st->ConOut;
 
-	Print(L"UEFI v%d.%d (%s, 0x%08X)\n", gST->Hdr.Revision >> 16, gST->Hdr.Revision & 0xFFFF,
-		gST->FirmwareVendor, gST->FirmwareRevision);
+    // Declare a variable to store the key pressed by the user
+    EFI_INPUT_KEY finalthingkey;
 
-	Status = LibGetSystemConfigurationTable(&SMBIOS3TableGuid, (VOID**)&Smbios3Table);
-	if (Status == EFI_SUCCESS) {
-		Smbios.Hdr = (SMBIOS_HEADER*)Smbios3Table->TableAddress;
-		MaximumSize = (UINTN)Smbios3Table->TableMaximumSize;
-	} else {
-		Status = LibGetSystemConfigurationTable(&SMBIOSTableGuid, (VOID**)&SmbiosTable);
-		if (EFI_ERROR(Status))
-			return EFI_NOT_FOUND;
-		Smbios.Hdr = (SMBIOS_HEADER*)(UINTN)SmbiosTable->TableAddress;
-		MaximumSize = (UINTN)SmbiosTable->TableLength;
-	}
+    // Clear the screen
+    ConOut->ClearScreen(ConOut);
 
-	while ((Smbios.Hdr->Type != 0x7F) && (Found < 2)) {
-		Raw = Smbios.Raw;
-		if (Smbios.Hdr->Type == 0) {
-			Print(L"%a %a\n", LibGetSmbiosString(&Smbios, Smbios.Type0->Vendor),
-				LibGetSmbiosString(&Smbios, Smbios.Type0->BiosVersion));
-			Found++;
-		}
-		if (Smbios.Hdr->Type == 1) {
-			Print(L"%a %a\n", LibGetSmbiosString(&Smbios, Smbios.Type1->Manufacturer),
-				LibGetSmbiosString(&Smbios, Smbios.Type1->ProductName));
-			Found++;
-		}
-		LibGetSmbiosString(&Smbios, -1);
-		ProcessedSize += (UINTN)Smbios.Raw - (UINTN)Raw;
-		if (ProcessedSize > MaximumSize) {
-			Print(L"%EAborting system report due to noncompliant SMBIOS%N\n");
-			return EFI_ABORTED;
-		}
-	}
+    // Display a simple message indicating that the program is running
+    ConOut->OutputString(ConOut, L"It Boots\n\r");
 
-	SecureBoot = LibGetVariable(L"SecureBoot", &EfiGlobalVariable);
-	SetupMode = LibGetVariable(L"SetupMode", &EfiGlobalVariable);
-	SecureBootStatus = ((SecureBoot != NULL) && (*SecureBoot != 0)) ? 1 : 0;
-	// You'd expect UEFI platforms to properly clear SetupMode after they
-	// installed all the certs... but most of them don't. Hence Secure Boot
-	// disabled having precedence over SetupMode. Looking at you OVMF!
-	if ((SetupMode != NULL) && (*SetupMode != 0))
-		SecureBootStatus *= -1;
-	// Wasteful, but we can't highlight "Enabled"/"Setup" from a %s argument...
-	if (SecureBootStatus > 0)
-		Print(L"Secure Boot status: %HEnabled%N\n");
-	else if (SecureBootStatus < 0)
-		Print(L"Secure Boot status: %ESetup%N\n");
-	else
-		Print(L"Secure Boot status: Disabled\n");
+    // Display the firmware revision information
+    ConOut->OutputString(ConOut, L"Revision: ");
+    ConOut->OutputString(ConOut, &st->FirmwareRevision);
+    ConOut->OutputString(ConOut, L"\n\r");
 
-	return EFI_SUCCESS;
-}
+    // Display the firmware vendor information
+    ConOut->OutputString(ConOut, L"Vendor: ");
+    ConOut->OutputString(ConOut, st->FirmwareVendor);
+    ConOut->OutputString(ConOut, L"\n\r");
 
-// Application entrypoint (must be set to 'efi_main' for gnu-efi crt0 compatibility)
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
-	UINTN Event;
+    // Prompt the user to press a key to exit or press Esc to shut down
+    ConOut->OutputString(ConOut, L"Press a key to exit, or esc to shutdown\n\r");
 
-#if defined(_GNU_EFI)
-	InitializeLib(ImageHandle, SystemTable);
-#endif
+    // Wait for a key press
+    st->BootServices->WaitForEvent(1, &(st->ConIn->WaitForKey), NULL);
 
-	// The platform logo may still be displayed → remove it
-	SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+    // Read the key pressed by the user
+    st->ConIn->ReadKeyStroke(st->ConIn, &finalthingkey);
 
-	/*
-	 * In addition to the standard %-based flags, Print() supports the following:
-	 *   %N       Set output attribute to normal
-	 *   %H       Set output attribute to highlight
-	 *   %E       Set output attribute to error
-	 *   %r       Human readable version of a status code
-	 */
-	Print(L"\n%H*** UEFI Simple (%s) ***%N\n\n", ArchName);
+    // Check if the pressed key is the Esc key
+    if (finalthingkey.ScanCode == SCAN_ESC)
+    {
+        // Display a message indicating that the system is shutting down
+        ConOut->OutputString(ConOut, L"Shutting Down");
 
-	PrintSystemInfo();
+        // Stall for 3 seconds (in microseconds) before shutting down
+        st->BootServices->Stall(3 * 1000000);
 
-	Print(L"\n%EPress any key to exit.%N\n");
-	SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
-	SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &Event);
-#if defined(_DEBUG)
-	// If running in debug mode, use the EFI shut down call to close QEMU
-	SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-#endif
+        // Perform a system shutdown
+        st->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+    }
+    else
+    {
+        // Display a message indicating that the program is exiting
+        ConOut->OutputString(ConOut, L"Exiting");
 
-	return EFI_SUCCESS;
+        // Stall for 3 seconds (in microseconds) before exiting
+        st->BootServices->Stall(3 * 1000000);
+
+        // Exit the EFI application
+        st->BootServices->Exit(ImageHandle, EFI_SUCCESS, 0, NULL);
+    }
+
+    // Return EFI_SUCCESS to indicate successful execution
+    return EFI_SUCCESS;
 }
