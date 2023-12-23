123f0c0f8bf454a4adfc6f379a5576fb6d940f6b
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-12-23 22:50:17 +0000

    made a simple cmdline system, it may create memleaks but oh well

--- main.c
+++ main.c
@@ -1,63 +1,116 @@
 #include <efi.h>
+#include <efilib.h>
+
+CHAR16* getcmd(EFI_SYSTEM_TABLE* st)
+{
+	CHAR16* cmdbuffer;
+	EFI_INPUT_KEY keystroke = {SCAN_NULL, CHAR_NULL};
+	UINTN index = 0;
+
+	st->BootServices->AllocatePool(EfiConventionalMemory, 4096, &cmdbuffer);
+
+	while (keystroke.UnicodeChar != CHAR_CARRIAGE_RETURN)
+	{
+		st->BootServices->WaitForEvent(1, &(st->ConIn->WaitForKey), NULL);
+		st->ConIn->ReadKeyStroke(st->ConIn, &keystroke);
+
+		st->ConOut->OutputString(st->ConOut, &keystroke.UnicodeChar);
+
+		cmdbuffer[index] = keystroke.UnicodeChar;
+		index++;
+	}
+	
+	cmdbuffer[index] = L'\0';
+	st->ConOut->OutputString(st->ConOut, L"\n\r");
+	st->ConOut->OutputString(st->ConOut, L"boutacopy");
+	CHAR16* out;
+	st->BootServices->AllocatePool(EfiConventionalMemory, (index + 1) * sizeof(CHAR16), &out);
+	st->ConOut->OutputString(st->ConOut, L"alocated");
+	for (UINTN i = 0; i <= index; i++) {
+		out[i] = cmdbuffer[i];
+	}
+	st->ConOut->OutputString(st->ConOut, L"coppied");
+	out[index] = L'\0';
+
+	keystroke.ScanCode = SCAN_NULL;
+	keystroke.UnicodeChar = CHAR_NULL;
+	st->ConOut->OutputString(st->ConOut, L"boutafree");
+	st->BootServices->FreePool(cmdbuffer);
+	st->ConOut->OutputString(st->ConOut, L"ohno");
+	return out;
+}
+
+EFI_STATUS cmdline(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* st)
+{
+	SIMPLE_TEXT_OUTPUT_INTERFACE* ConOut = st->ConOut;
+	ConOut->ClearScreen(ConOut);
+	ConOut->OutputString(ConOut, L"0:");
+
+	while (1)
+	{
+		CHAR16* command = getcmd(st);
+		ConOut->OutputString(ConOut, L"You typed: ");
+		for (int i = 0; command[i] != L'\0' || i >= 4096; i++)
+		{
+			CHAR16 charString[2] = { command[i], L'\0' };
+			ConOut->OutputString(ConOut, charString);
+		}
+		//st->BootServices->FreePool(command); //You may think that this creates a memory leak, maybe, but for some reason it makes the program halt, so...
+		ConOut->OutputString(ConOut, L"\n\r0:");
+		
+	}
+
+	return EFI_SUCCESS;
+}
 
 // Entry point for the EFI application
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* st)
 {
-    // Obtain a reference to the Console Output Interface
-    SIMPLE_TEXT_OUTPUT_INTERFACE* ConOut = st->ConOut;
+	// Obtain a reference to the Console Output Interface
+	SIMPLE_TEXT_OUTPUT_INTERFACE* ConOut = st->ConOut;
 
-    // Declare a variable to store the key pressed by the user
-    EFI_INPUT_KEY finalthingkey;
+	// Declare a variable to store the key pressed by the user
+	EFI_INPUT_KEY finalthingkey;
 
-    // Clear the screen
-    ConOut->ClearScreen(ConOut);
+	// Clear the screen
+	ConOut->ClearScreen(ConOut);
 
-    // Display a simple message indicating that the program is running
-    ConOut->OutputString(ConOut, L"It Boots\n\r");
+	// Display a simple message indicating that the program is running
+	ConOut->OutputString(ConOut, L"It Boots\n\r");
 
-    // Display the firmware revision information
-    ConOut->OutputString(ConOut, L"Revision: ");
-    ConOut->OutputString(ConOut, &st->FirmwareRevision);
-    ConOut->OutputString(ConOut, L"\n\r");
+	// Display the firmware vendor information
+	ConOut->OutputString(ConOut, L"Vendor: ");
+	ConOut->OutputString(ConOut, st->FirmwareVendor);
+	ConOut->OutputString(ConOut, L"\n\r");
 
-    // Display the firmware vendor information
-    ConOut->OutputString(ConOut, L"Vendor: ");
-    ConOut->OutputString(ConOut, st->FirmwareVendor);
-    ConOut->OutputString(ConOut, L"\n\r");
+	// Prompt the user to press a key to exit or press Esc to shut down
+	ConOut->OutputString(ConOut, L"Press a key to continue, or esc to shutdown\n\r");
 
-    // Prompt the user to press a key to exit or press Esc to shut down
-    ConOut->OutputString(ConOut, L"Press a key to exit, or esc to shutdown\n\r");
+	// Wait for a key press
+	st->BootServices->WaitForEvent(1, &(st->ConIn->WaitForKey), NULL);
 
-    // Wait for a key press
-    st->BootServices->WaitForEvent(1, &(st->ConIn->WaitForKey), NULL);
+	// Read the key pressed by the user
+	st->ConIn->ReadKeyStroke(st->ConIn, &finalthingkey);
 
-    // Read the key pressed by the user
-    st->ConIn->ReadKeyStroke(st->ConIn, &finalthingkey);
+	// Check if the pressed key is the Esc key
+	if (finalthingkey.ScanCode == SCAN_ESC)
+	{
+		// Display a message indicating that the system is shutting down
+		ConOut->OutputString(ConOut, L"Shutting Down");
 
-    // Check if the pressed key is the Esc key
-    if (finalthingkey.ScanCode == SCAN_ESC)
-    {
-        // Display a message indicating that the system is shutting down
-        ConOut->OutputString(ConOut, L"Shutting Down");
+		// Stall for 3 seconds (in microseconds) before shutting down
+		st->BootServices->Stall(3 * 1000000);
 
-        // Stall for 3 seconds (in microseconds) before shutting down
-        st->BootServices->Stall(3 * 1000000);
+		// Perform a system shutdown
+		st->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+	}
+	else
+	{
+		ConOut->OutputString(ConOut, L"Continuing\n\r");
+		st->BootServices->Stall(1000000);
+		cmdline(ImageHandle, st);
+	}
 
-        // Perform a system shutdown
-        st->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-    }
-    else
-    {
-        // Display a message indicating that the program is exiting
-        ConOut->OutputString(ConOut, L"Exiting");
-
-        // Stall for 3 seconds (in microseconds) before exiting
-        st->BootServices->Stall(3 * 1000000);
-
-        // Exit the EFI application
-        st->BootServices->Exit(ImageHandle, EFI_SUCCESS, 0, NULL);
-    }
-
-    // Return EFI_SUCCESS to indicate successful execution
-    return EFI_SUCCESS;
+	// Return EFI_SUCCESS to indicate successful execution
+	return EFI_SUCCESS;
 }
